89f8726c92a8466724dc3f75402e3e72c2a29419
Lola Rigaut-Luczak <rllola80@gmail.com>
2016-08-06 18:15:12 +0200

    Standard javascript for Login

--- app/components/login.js
+++ app/components/login.js
@@ -1,11 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ZeroFrame from 'zeroframe'
 
 class Login extends Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleClick () {
+    ZeroFrame.cmd('certSelect', [['zeroid.bit']], null)
+  }
+
+  render () {
     return (
-      <a className="nav-link" href="#Select+user" id="select_user" onclick="return ZeroTube.selectUser()">Select user</a>
-    );
+      <a className='nav-link' href='#' id='select_user' onClick={this.handleClick}>Select user</a>
+    )
   }
 };
 
-export default Login;
+function mapStateToProps (state) {
+  return {
+    site: state.site
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(Login)
