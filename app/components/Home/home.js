d7377164939524d275f9208c0efebe93bcb93bc1
Lola Rigaut-Luczak <rllola80@gmail.com>
2016-08-06 20:53:16 +0200

    Standard javascript

--- app/components/Home/home.js
+++ app/components/Home/home.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Home extends Component {
-  render() {
+  render () {
     let style = {
-      marginTop : '20%',
+      marginTop: '20%'
     }
     return (
       <img style={style} src="public/img/zero_degrade.png" className="img-fluid m-x-auto d-block" ></img>
-    );
+    )
   }
 };
 
-export default Home;
+export default Home
