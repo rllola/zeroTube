f6bad0b0dcd478e63cbc711a16a6fa098f5b732d
Alexandre <Seldoon182@hotmail.com>
2016-08-08 04:44:44 +0200

    Header facelift

--- app/components/main.layout.js
+++ app/components/main.layout.js
@@ -9,21 +9,21 @@ class MainLayout extends Component {
     return (
       <div className="app">
         <nav className="navbar navbar-light bg-faded">
-          <Link to={`${Constants.APP_ID}`} className="navbar-brand col-xs-1"><img src="public/img/zero_bleu.png" height="35" /></Link>
+          <Link to={`${Constants.APP_ID}`} className="navbar-brand col-xs-1"><img src="public/img/zerotube-logo.svg" height="35" /></Link>
           <div>
-            <form className="form-inline col-xs-6" name="searchForm" onsubmit="ZeroTube.submit(event);">
-              <input className="form-control" id="query" type="text" name="query" placeholder="search" />
-              <input className="btn btn-primary-outline" type="submit" value="Submit" />
+            <form className="form-inline" name="searchForm" onsubmit="ZeroTube.submit(event);">
+              <input className="form-control" id="query" type="text" name="query" placeholder="Search" />
+              <input className="btn-glass" type="submit" value="Submit" />
             </form>
           </div>
           <ul className="nav navbar-nav col-xs-3">
-            <li className="nav-item">
-              <Login />
-            </li>
-            <li className="nav-item">
+            <li className="nav-item btn-upload">
               <a className="nav-link" data-toggle="modal" data-target="#myModal" href="#">Upload</a>
             </li>
-            <li className="nav-item">
+            <li className="nav-item btn-user">
+              <Login />
+            </li>
+            <li className="nav-item btn-help">
               <Link className="nav-link" to="how">How it works</Link>
             </li>
           </ul>
